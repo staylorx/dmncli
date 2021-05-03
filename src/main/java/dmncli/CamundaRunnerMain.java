2a838f37efef3f6327451d9b85344bad33acdb53
Steve Taylor <staylorx@gmail.com>
2021-05-03 11:47:07 -0600

    Change formatter to quiet linter

--- src/main/java/dmncli/CamundaRunnerMain.java
+++ src/main/java/dmncli/CamundaRunnerMain.java
@@ -9,16 +9,11 @@ import picocli.CommandLine.Parameters;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.concurrent.Callable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.commons.io.FileUtils;
 
-@Command(name = "dmncli", mixinStandardHelpOptions = true, version = "dmncli 1.0",
-        description = "Decides inputs against a DMN decision.")
+@Command(name = "dmncli", mixinStandardHelpOptions = true, version = "dmncli 1.0", description = "Decides inputs against a DMN decision.")
 class CamundaRunnerMain implements Callable<Integer> {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -26,16 +21,17 @@ class CamundaRunnerMain implements Callable<Integer> {
     @Parameters(index = "0", description = "The DMN file.")
     private File file;
 
-    @Option(names = {"-d", "--decision"}, description = "The Decision label")
+    @Option(names = { "-d", "--decision" }, description = "The Decision label")
     private String decision = "";
-    @Option(names = {"-i", "--inputVars"}, description = "Input variables JSON string")
+    @Option(names = { "-i", "--inputVars" }, description = "Input variables JSON string")
     private String inputVars = "";
-    @Option(names = {"-if", "--inputVarsFile"}, description = "Input variable file")
+    @Option(names = { "-if", "--inputVarsFile" }, description = "Input variable file")
     private File inputVarFile;
-    @Option(names = {"-of", "--outputVarsFile"}, description = "Output variable file")
+    @Option(names = { "-of", "--outputVarsFile" }, description = "Output variable file")
     private File outputVarFile;
 
-    // this example implements Callable, so parsing, error handling and handling user
+    // this example implements Callable, so parsing, error handling and handling
+    // user
     // requests for usage help or version help can be done with one line of code.
     public static void main(String... args) {
         int exitCode = new CommandLine(new CamundaRunnerMain()).execute(args);
@@ -44,10 +40,10 @@ class CamundaRunnerMain implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        logger.debug("DMN File: " + file.getAbsolutePath());
-        logger.debug("Decision: " + decision);
-        logger.debug("InputVar: " + inputVars);
-        logger.debug("InputVarFile: " + inputVarFile.getAbsolutePath());
+        logger.debug("DMN File: {}", file.getAbsolutePath());
+        logger.debug("Decision: {}", decision);
+        logger.debug("InputVar: {}", inputVars);
+        logger.debug("InputVarFile: {}", inputVarFile.getAbsolutePath());
 
         CamundaRunner camundaRunner = new CamundaRunner(file.getAbsolutePath());
 
@@ -65,7 +61,7 @@ class CamundaRunnerMain implements Callable<Integer> {
         if (outputVarFile != null) {
             try {
                 FileUtils.writeStringToFile(outputVarFile, resultJsonString, StandardCharsets.UTF_8);
-                logger.info("Output written to " + outputVarFile.getAbsolutePath());
+                logger.info("Output written to {}", outputVarFile.getAbsolutePath());
             } catch (Exception e) {
                 logger.error(e.getMessage());
                 return 1;
